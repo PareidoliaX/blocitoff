ede7a4e04b17494fbb185f9b8c6b333ea45b0910
Dan Bennett <dmbennet@gmail.com>
2015-01-27 19:51:26 +0000

    added Ajax desctruction

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,23 @@ class ItemsController < ApplicationController
     end
   end
   
+  def destroy
+    @list = current_user.list
+    @item = @list.items.find(params[:id])
+
+    if @item.destroy
+      flash[:notice] = "Congrats on Finishing the item!"
+    else
+      flash[:error] = "Couldnt mark the item as completed"
+    end
+ 
+    respond_with(@item) do |format|
+      format.html { redirect_to @list }
+    end
+    
+
+  end
+  
   
   private 
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users # , :controllers => { :registrations => 'users/registrations' } 
   
   resources :lists, except: [:index] do
-    resources :items, only: [:create]
+    resources :items, only: [:create, :destroy]
   end
 
   root to: 'shallows#home'
